5fc7bc6d583f25ad9e4df39fa0df6d344ce9f5ec
Furkan Peker <peker486@gmail.com>
2022-05-15 11:16:06 +0300

    Kullanıcı ekleme aktif

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {addUser} from "./features/Users";
 
 function App() {
+  const userList = useSelector((state) => state.users.value);
+
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="addUser">
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            dispatch(
+              addUser({
+                id: userList[userList.length - 1].id + 1,
+                name,
+                username,
+              })
+            );
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Add User
+        </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1> {user.name}</h1>
+              <h2> {user.username}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/FakeData.js
@@ -0,0 +1,22 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Furkan Peker",
+    username: "furkanpeker",
+  },
+  {
+    id: 2,
+    name: "Hakan Yıldırım",
+    username: "hakanyildirim",
+  },
+  {
+    id: 3,
+    name: "Onder Senel",
+    username: "ondersenel",
+  },
+  {
+    id: 4,
+    name: "Aslan Eraslan",
+    username: "aslaneraslan",
+  },
+];
